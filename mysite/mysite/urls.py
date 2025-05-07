fab20b845478706202edeb06ffa5d3c08017bf09
Syimyk0107 <syimykkubandykov01@gmail.com>
2025-05-07 08:13:10 +0000

    Q/CH

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,15 @@
+# mysite/urls.py
+# from django.contrib import admin
+# from django.urls import include, path
+#
+# urlpatterns = [
+#     path("polls/", include("polls.urls")),
+#     path("admin/", admin.site.urls),
+# ]
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
